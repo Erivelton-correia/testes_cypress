0cf5cceeba999a182a16d8b9cab74ef0a6b7a95c
ercrrc <134560333+ercrrc@users.noreply.github.com>
2024-03-20 10:12:12 -0300

    correcao

--- cypress/e2e/editando.cy.js
+++ cypress/e2e/editando.cy.js
@@ -1,21 +1,14 @@
 /// <reference types="cypress" />
 
-describe('Testes para a pagina de candidatura', () => {
+describe('Testes para editar contatos', () => {
     beforeEach(() => {
         cy.visit('https://agenda-contatos-react.vercel.app/')
     })
 
-    it('Deve preencher o forulario de inscricao', () => {
+    it('Deve editar os campos', () => {
         cy.get('input[type="text"]').type('erivelton correia')
         cy.get('input[type="email"]').type('eriveltoncorreia@teste.com')
         cy.get('input[type="tel"]').type('44 12345678')
         cy.get('.adicionar').click()
-
-        cy.on('window:alert', (conteudo) => {
-            expect(conteudo).contain('Obrigado pela candidatura!')
-            cy.get(':nth-child(2) > .sc-gueYoa > .edit')
-        })
-
-        cy.screenshot('tela-inscricao-preenchido')
     })
 })
